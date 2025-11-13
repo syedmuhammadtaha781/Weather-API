33ed79126b4a85e4510a0e6beed000d3fd731f55
Syed Muhammad Taha <syedtaha456774@gmail.com>
2025-11-14 01:00:34 +0500

    changes

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 let city = document.getElementById("city");
 let btn = document.getElementById("btn");
 let addin = document.querySelector(".addin");
-let heading = document.getElementById("heading");
-let temp = document.getElementById("temp");
-let Feel_like = document.getElementById("feel-like");
-let strong1 = document.getElementById("strong1");
-let strong2 = document.getElementById("strong2");
 btn.addEventListener("click",()=>{
     let t_city = city.value.trim();
     if (!t_city) {
@@ -16,6 +11,8 @@ btn.addEventListener("click",()=>{
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${t_city}&appid=20bf211dc0a1a61bb923e4adc757173e`)
     .then(resp => resp.json())
     .then(data => {
+        console.log(data);
+        
        if (data.message == "city not found") {
         alert("Please Enter correct city name")
         strong1.textContent = ""; 
@@ -27,15 +24,18 @@ btn.addEventListener("click",()=>{
         return;
        }
        addin.style.backgroundColor = "white";
-       strong1.textContent = "Temperature:";
-       strong2.textContent = "Feel-like:";
        let temp_c = (data.main.temp - 273).toFixed(1);
        let feels_c = (data.main.feels_like - 273).toFixed(1);
-       heading.textContent = data.name;
-       temp.innerHTML = `${temp_c}<sup>o</sup>C`;
-       Feel_like.innerHTML = `${feels_c}<sup>o</sup>C`;
+    addin.innerHTML = `
+    <h2>${data.name}</h2>
+    <p><strong>Temperature: </strong>${temp_c}<sup>o</sup>C</p>
+    <p><strong>Feel-like: </strong>${feels_c}<sup>o</sup>C</p>
+    <p><strong>Weather: </strong>${data.weather[0].main}</p>
+    <p><strong>Wind: </strong>${data.wind.speed} m/s from ${data.wind.deg}<sup>o</sup></p>
+    <p><strong>Visibility: </strong>${data.visibility}</p>
+    <p><strong>Cloud Cover: </strong>${data.clouds.all}%</p>
+    `;
        city.value = "";
     })
-    .catch(err => console.log(err))
-    
+    .catch(err => console.log(err))  
 })
